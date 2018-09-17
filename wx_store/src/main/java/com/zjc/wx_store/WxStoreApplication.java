9ebb8e918541042fe35d7d460423be45c4bd351d
zhoujiacan <zhoujiacan@sanduspace.cn>
2018-09-17 11:06:30 +0800

    init project dir

---
+++ wx_store/src/main/java/com/zjc/wx_store/WxStoreApplication.java
@@ -0,0 +1,12 @@
+package com.zjc.wx_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxStoreApplication.class, args);
+	}
+}
---
+++ wx_store/src/test/java/com/zjc/wx_store/WxStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zjc.wx_store;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WxStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
